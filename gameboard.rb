f682a3cdec1e0f81866d9e1882bd6ed34766b6ea
Benjamin Kudria <ben@kudria.net>
2010-12-04 15:33:58 -0800

    Ship placement logic

--- gameboard.rb
+++ gameboard.rb
@@ -25,17 +25,26 @@ class Gameboard
 			raise "Invalid ship position"
 		end
 
-		if (position.first + (orientation == :horiz ? ship.length : 0) > @board.column_size) ||
-				(position.last + (orientation == :vert  ? ship.length : 0) > @board.row_size)
+		if (position.first + (orientation == :vert  ? ship.length : 0) > @board.column_size) ||
+				(position.last + (orientation == :horiz ? ship.length : 0) > @board.row_size)
 			raise ShipPlacementException
 		end
 
-		@ships[(@ships.keys.min || -1) + 1] = {
+		ship_id = (@ships.keys.min || 10) + 1
+		@ships[ship_id] = {
 			:ship => ship,
 			:orientation => orientation,
 			:row => position.first,
 			:col => position.last
 		}
+
+		ship.length.times do |index|
+			if orientation == :vert
+				@board.send(:set_element, position.first + index, position.last, ship_id)
+			else
+				@board.send(:set_element, position.first, position.last + index, ship_id)
+			end
+		end
 	end
 
 	def position_to_string(row, col)
@@ -43,13 +52,13 @@ class Gameboard
 		if cell == WATER
 			STATES[WATER][:icon]
 		else
-			ship_placement = SHIPS[cell]
+			ship_placement = @ships[cell]
 			if ship_placement[:orientation] == :vert
 				ship_section = ship_placement[:row] - row
 			else
 				ship_section = ship_placement[:col] - col
 			end
-			STATES[ship_placement[:ship][ship_section]]
+			STATES[ship_placement[:ship][ship_section]][:icon]
 		end
 	end
 
