37d294edbf21be8e076100d29bbe1f6528d1d9c6
Jeff Shiau <jeff@causes.com>
2010-12-04 16:26:49 -0800

    gameinput functions for ship placement and attack coordinates

--- gameinput.rb
+++ gameinput.rb
@@ -1,10 +1,18 @@
+require 'rubygems'
+require 'highline/import'
+
 class GameInput
 	class << self
 		def get_ship_placement
-			ask "input ship placement: "
+			input = ask "input ship placement as x,y,orientation: "
+			raw = input.split( ',' )
+			coords = raw.first(2).map(&:to_i)
+			orientation = raw.last
+			[coords, orientation]
 		end
 		def get_attack_coordinates
-			ask "input coordinates: "
+			input = ask "input coordinates as x,y: "
+			input.split(',').map(&:to_i)
 		end
 	end
 end
