657e31e3b01142788aa56314845044b1783025e1
Jeff Shiau <jeff@causes.com>
2010-12-04 16:31:50 -0800

    return compliant orientation values

--- gameinput.rb
+++ gameinput.rb
@@ -7,7 +7,14 @@ class GameInput
 			input = ask "input ship placement as x,y,orientation: "
 			raw = input.split( ',' )
 			coords = raw.first(2).map(&:to_i)
-			orientation = raw.last
+			raw_orientation = raw.last
+			if raw_orientation == 'h'
+				orientation = :horiz
+			elsif raw_orientation == 'v'
+				orientation = :vert
+			else
+				orientation = nil
+			end
 			[coords, orientation]
 		end
 		def get_attack_coordinates
