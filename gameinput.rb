0da12fea9380df755896c4c46492087116dd6e36
Jeff Shiau <jeff@causes.com>
2010-12-04 16:57:49 -0800

    fancier ship placement prompt, get player name

--- gameinput.rb
+++ gameinput.rb
@@ -3,8 +3,12 @@ require 'highline/import'
 
 class GameInput
 	class << self
-		def get_ship_placement
-			input = ask "input ship placement as x,y,orientation: "
+		def get_player_name
+			ask "A CHALLENGER APPEARS!  by the name of... "
+		end
+
+		def get_ship_placement( ship_name, ship_length )
+			input = ask "place your #{ship_name} (#{ship_length} tiles)  x,y,orientation: "
 			raw = input.split( ',' )
 			coords = raw.first(2).map(&:to_i)
 			raw_orientation = raw.last
@@ -17,6 +21,7 @@ class GameInput
 			end
 			[coords, orientation]
 		end
+
 		def get_attack_coordinates
 			input = ask "input coordinates as x,y: "
 			input.split(',').map(&:to_i)
