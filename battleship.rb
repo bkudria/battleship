0a1015a051b7a7b22a01406bf1a4984ed03cd5a3
Benjamin Kudria <ben@kudria.net>
2010-12-04 16:08:21 -0800

    Optionally hide ships when printing board

--- battleship.rb
+++ battleship.rb
@@ -6,6 +6,12 @@ require './player'
 require './gameinput'
 
 
+board =  Gameboard.new(10)
+board.place_ship(Ship.new(Ship::CRUISER), [2, 2], :vert)
+#puts board.fire(2, 3)
+puts board.board_display(true)
+
+exit
 
 BOARD_SIZE = 10
 
--- gameboard.rb
+++ gameboard.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 require 'matrix'
 
-class ShipPlacementException < RuntimeError
-end
+class ShipPlacementException < RuntimeError; end
+class BoardException < RuntimeError; end
 
 class Gameboard
 	WATER    = 0
@@ -21,16 +21,16 @@ class Gameboard
 
 	def place_ship(ship, position = [0, 0], orientation)
 		if ![:vert, :horiz].include? orientation
-			raise "Invalid ship orientation"
+			raise ShipPlacementException.new "Invalid ship orientation"
 		end
 
 		if @board[*position].nil?
-			raise "Invalid ship position"
+			raise ShipPlacementException.new "Invalid ship position"
 		end
 
 		if (position.first + (orientation == :vert  ? ship.length : 0) > @board.column_size) ||
 				(position.last + (orientation == :horiz ? ship.length : 0) > @board.row_size)
-			raise ShipPlacementException
+			raise ShipPlacementException.new "Ship doesn't fit on the board there"
 		end
 
 		ship_id = (@ships.keys.min || 10) + 1
@@ -50,7 +50,53 @@ class Gameboard
 		end
 	end
 
-	def position_to_string(row, col)
+	def [](row, col)
+		if @board[row, col].nil?
+			raise BoardException.new "Invalid position"
+		end
+
+		cell = @board[row, col]
+
+		if cell == WATER
+			return false
+		else
+			ship_placement = @ships[cell]
+			if ship_placement[:orientation] == :vert
+				ship_section = ship_placement[:row] - row
+			else
+				ship_section = ship_placement[:col] - col
+			end
+
+			if ship_section == WRECKAGE
+				return false
+			else
+				return cell
+			end
+		end
+	end
+
+	def fire(row, col)
+		cell = self[row, coll]
+		if cell == false
+			return false
+		else
+			ship_placement = @ships[cell]
+			if ship_placement[:orientation] == :vert
+				section_index = ship_placement[:row] - row
+			else
+				section_index = ship_placement[:col] - col
+			end
+
+			section_result = ship_placement[:ship].destroy(section_index)
+			if ship_placement[:ship].destroyed?
+				return :ship_destroyed
+			else
+				return section_result
+			end
+		end
+	end
+
+	def position_to_string(row, col, hide_ships = false)
 		cell = @board[row,col]
 		if cell == WATER
 			STATES[WATER][:icon]
@@ -61,11 +107,23 @@ class Gameboard
 			else
 				ship_section = ship_placement[:col] - col
 			end
-			STATES[ship_placement[:ship][ship_section]][:icon]
+			if hide_ships
+				if ship_placement[:ship][ship_section] != WRECKAGE
+					STATES[WATER][:icon]
+				else
+					STATES[ship_placement[:ship][ship_section]][:icon]
+				end
+			else
+				STATES[ship_placement[:ship][ship_section]][:icon]
+			end
 		end
 	end
 
 	def to_s
+		self.board_display(false)
+	end
+
+	def board_display(hide_ships = false)
 		output = "\n"
 
 		print_board = Matrix.build(@board.row_size + 1, @board.column_size + 1) do |row, col|
@@ -76,7 +134,7 @@ class Gameboard
 			elsif col == 0
 				row - 1
 			else
-				position_to_string(row - 1, col - 1)
+				position_to_string(row - 1, col - 1, hide_ships)
 			end
 		end
 
