a7742a328916627192cc0a11e19c60c275f58dce
Jeff Shiau <jeff@causes.com>
2010-12-04 17:22:47 -0800

    flesh out the player, prep to integrate with firing logic

--- player.rb
+++ player.rb
@@ -7,15 +7,32 @@ class Player
   end
 
 	def is_dead?
-
+		nil
 	end
 
 	def	turn( other_player )
+		puts "========== #{self.name}'s turn! ========="
+		puts "your board:"
+		self.board.board_display()
+		puts "your opponent's board:"
+		other_player.board.board_display()
+
 		coordinates = GameInput.get_attack_coordinates
 		outcome = other_player.take_fire( coordinates )
-		self.update_board( coordinates, outcome )
 
-		nil
+    if outcome == Gameboard::WATER
+      puts "you missed!"
+    elsif outcome == Gameboard::SHIP
+      puts "HIT!"
+    elsif outcome == Gameboard::WRECKAGE
+      puts "you demolished that tile already!"
+    end
+
+		if other_player.is_dead?
+			return false
+		else
+			return true
+		end
 	end
 
 	def take_fire( coordinates )
